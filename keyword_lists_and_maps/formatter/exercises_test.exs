0487bd8a8e793b955ddac628efd6686accbca97f
waseem-medhat <wipdev.tech@gmail.com>
2025-08-09 16:16:59 +0300

    Add exercises for "Keyword lists and maps"

---
+++ keyword_lists_and_maps/formatter/exercises_test.exs
@@ -0,0 +1,26 @@
+defmodule Spirit.KeywordsAndMaps.FormatterTest do
+  use ExUnit.Case
+  doctest Spirit.KeywordsAndMaps.Formatter
+
+  alias Spirit.KeywordsAndMaps.Formatter
+
+  test "default_config/0" do
+    config = Formatter.default_config()
+    assert Keyword.get(config, :capitalize)
+    assert not Keyword.get(config, :wrap)
+    assert Keyword.get(config, :max_length) === 60
+  end
+
+  test "format/2" do
+    long_line =
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" <>
+        "bbbbbbbbbbbbbb"
+
+    expected =
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" <>
+        "\nbbbbbbbbbbbbbb"
+
+    result = Formatter.format(long_line, wrap: true, capitalize: false)
+    assert result === expected
+  end
+end
